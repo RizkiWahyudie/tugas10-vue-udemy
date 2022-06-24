2703af750c5b8f7123933563128163f5b961873f
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-06-24 14:23:56 +0700

    change title

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -4,7 +4,7 @@
         <div class="container flex flex-wrap justify-between items-center mx-auto">
             <a href="https://flowbite.com/" class="flex items-center">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">VueData</span>
             </a>
             <div class="flex items-center md:order-2">
                 <button type="button" class="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
